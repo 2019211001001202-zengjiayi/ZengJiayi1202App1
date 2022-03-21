d8ef023e50882b762942a163c0b5c305e8b90e56
2019211001001202-zengjiayi <80387132+2019211001001202-zengjiayi@users.noreply.github.com>
2022-03-21 14:51:21 +0800

    Add files via upload

---
+++ ZengJiayi1202App/W1HW2App.1/DayPrinter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW2App._1
+{
+    internal class DayPrinter
+    {
+        static void Main(string[] args)
+        {
+            String[] days = { "Manday,Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday" };
+            for (int i = 0; i < days.Length; i++) 
+            { Console.WriteLine(days[i]); }
+        }
+    }
+}
